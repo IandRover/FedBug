3b0a6c4f91baa14bfcb86612b4258137de14b021
ChiaHsiang0326 <0102030405jacky@gmail.com>
2023-05-18 20:23:58 +0800

    Update utils_methods.py

--- utils_methods.py
+++ utils_methods.py
@@ -10,35 +10,28 @@ import datetime, time
 def df_append(df, data): df.loc[len(df)] = data
 
 ### Methods
-def train(args, data_obj, model_func, init_model, rand_seed=0):
+def train(args, data_obj, model_func, init_model):
     
     memory = pd.DataFrame(columns=['task', 'mode', "balance", "distribution", "n_clients", "act_prob", "n_epochs",
                                    "seed", "epoch", "a1", "a2", "a3", "a4", "l1", "l2", "l3", "l4"])
 
     n_clnt=data_obj.n_client
     clnt_x = data_obj.clnt_x; clnt_y=data_obj.clnt_y
-    print(clnt_x.shape)
     cent_x, cent_y = np.concatenate(clnt_x, axis=0), np.concatenate(clnt_y, axis=0)
     
     weight_list = np.asarray([len(clnt_y[i]) for i in range(n_clnt)])
-    if args.mode in ["fedavg", "fedexp", "fedcm", "feddecorr", "fedavg1", "fedavg2", "fedavg3", "fedavg4", "fedavg5"]:
-        weight_list = weight_list.reshape((n_clnt, 1))
-    elif args.mode in ["scaffold", "feddyn", "fedadam", "fedadagrad", "fedavgm", "feddyn2"]:
-        weight_list = weight_list / np.sum(weight_list) * n_clnt # normalize it
-    elif args.mode in ["fedprox"]:
-        weight_list = weight_list.reshape((n_clnt, 1))
+    if args.mode in ["fedavg", "fedexp", "fedcm", "feddecorr"]: weight_list = weight_list.reshape((n_clnt, 1))
+    elif args.mode in ["scaffold", "feddyn", "fedadam", "fedadagrad", "fedavgm"]: weight_list = weight_list / np.sum(weight_list) * n_clnt
+    elif args.mode in ["fedprox"]: weight_list = weight_list.reshape((n_clnt, 1))
         
-    args.n_par = n_par = len(get_mdl_params([model_func()])[0])
-
+    n_par = len(get_mdl_params([model_func()])[0])
     init_par_list=get_mdl_params([init_model], n_par)[0]
     clnt_params_list=np.ones(n_clnt).astype('float32').reshape(-1, 1) * init_par_list.reshape(1, -1) # n_clnt X n_par
     clnt_models = list(range(n_clnt))
     avg_model = model_func().to(args.device)
-    if args.model_name in ["mobilenetv2"]: avg_model.load_state_dict(copy.deepcopy(dict(init_model.named_parameters())), strict=False)
-    else: avg_model.load_state_dict(copy.deepcopy(dict(init_model.named_parameters())))
+    avg_model.load_state_dict(copy.deepcopy(dict(init_model.named_parameters())))
     all_model = model_func().to(args.device)
-    if args.model_name in ["mobilenetv2"]: all_model.load_state_dict(copy.deepcopy(dict(init_model.named_parameters())), strict=False)
-    else: all_model.load_state_dict(copy.deepcopy(dict(init_model.named_parameters())))
+    all_model.load_state_dict(copy.deepcopy(dict(init_model.named_parameters())))
         
     if args.mode == "scaffold":
         state_param_list = np.zeros((n_clnt+1, n_par)).astype('float32')
@@ -57,7 +50,6 @@ def train(args, data_obj, model_func, init_model, rand_seed=0):
     for i in range(args.com_amount):
         
         args.global_epoch = i
-
         inc_seed = 0
         while(True):
             np.random.seed(i +  inc_seed* 1000 + args.seed * 10000000)
@@ -75,15 +67,12 @@ def train(args, data_obj, model_func, init_model, rand_seed=0):
             for clnt in selected_clnts:
                 trn_x, trn_y = clnt_x[clnt], clnt_y[clnt]
                 clnt_models[clnt] = model_func().to(args.device)
-                if args.model_name in ["mobilenetv2"]: clnt_models[clnt].load_state_dict(copy.deepcopy(dict(avg_model.named_parameters())), strict=False)
-                else: clnt_models[clnt].load_state_dict(copy.deepcopy(dict(avg_model.named_parameters())))
+                clnt_models[clnt].load_state_dict(copy.deepcopy(dict(avg_model.named_parameters())))
                 
                 for params in clnt_models[clnt].parameters(): params.requires_grad = True
-                if args.mode in ["fedavg", "fedavg1", "fedavg2", "fedavg3", "fedavg4", "fedavg5"]: clnt_models[clnt] = train_model(args, clnt_models[clnt], trn_x, trn_y, 
-                                                                        data_obj.dataset)
-                if args.mode == "feddecorr": clnt_models[clnt] = train_feddecorr_model(args, clnt_models[clnt], trn_x, trn_y, 
-                                                                        data_obj.dataset)
-                if args.mode == "fedprox": clnt_models[clnt] = train_fedprox_mdl(args, clnt_models[clnt], avg_model_param_tensor, args.mu, trn_x, trn_y, data_obj.dataset)
+                if args.mode in ["fedavg"]: clnt_models[clnt] = train_model(args, clnt_models[clnt], trn_x, trn_y)
+                if args.mode == "feddecorr": clnt_models[clnt] = train_feddecorr_model(args, clnt_models[clnt], trn_x, trn_y)
+                if args.mode == "fedprox": clnt_models[clnt] = train_fedprox_mdl(args, clnt_models[clnt], avg_model_param_tensor, args.mu, trn_x, trn_y)
                 clnt_params_list[clnt] = get_mdl_params([clnt_models[clnt]], n_par)[0]
 
             avg_mdl_param = np.mean(clnt_params_list[selected_clnts], axis = 0)
@@ -97,8 +86,7 @@ def train(args, data_obj, model_func, init_model, rand_seed=0):
                 clnt_models[clnt].load_state_dict(copy.deepcopy(dict(avg_model.named_parameters())))
                 for params in clnt_models[clnt].parameters(): params.requires_grad = True
 
-                clnt_models[clnt] = train_model(args, clnt_models[clnt], trn_x, trn_y, 
-                                                data_obj.dataset)
+                clnt_models[clnt] = train_model(args, clnt_models[clnt], trn_x, trn_y)
                 clnt_params_list[clnt] = get_mdl_params([clnt_models[clnt]], n_par)[0]
 
             avg_model_param = get_mdl_params([avg_model], n_par)
@@ -114,8 +102,7 @@ def train(args, data_obj, model_func, init_model, rand_seed=0):
                 clnt_models[clnt] = model_func().to(args.device)
                 clnt_models[clnt].load_state_dict(copy.deepcopy(dict(avg_model.named_parameters())))
                 for params in clnt_models[clnt].parameters(): params.requires_grad = True
-                clnt_models[clnt] = train_model(args, clnt_models[clnt], trn_x, trn_y, 
-                                                data_obj.dataset)
+                clnt_models[clnt] = train_model(args, clnt_models[clnt], trn_x, trn_y)
                 clnt_params_list[clnt] = get_mdl_params([clnt_models[clnt]], n_par)[0]
 
             avg_model_param = get_mdl_params([avg_model], n_par)[0]
@@ -142,9 +129,9 @@ def train(args, data_obj, model_func, init_model, rand_seed=0):
                 # Scale down c
                 state_params_diff_curr = torch.tensor(state_param_list[-1] - state_param_list[clnt], dtype=torch.float32, device=args.device)
                 clnt_models[clnt] = train_scaffold_mdl(args, clnt_models[clnt], model_func, state_params_diff_curr, trn_x, trn_y, 
-                                                       args.n_minibatch, data_obj.dataset)
+                                                       args.n_minibatch)
                 curr_model_param = get_mdl_params([clnt_models[clnt]], n_par)[0]
-                new_c = state_param_list[clnt] - state_param_list[-1] + 1/args.n_minibatch/learning_rate * (prev_params - curr_model_param)
+                new_c = state_param_list[clnt] - state_param_list[-1] + 1/args.n_minibatch/args.lr * (prev_params - curr_model_param)
                 # Scale up delta c
                 delta_c_sum += (new_c - state_param_list[clnt])
                 state_param_list[clnt] = new_c
@@ -155,7 +142,7 @@ def train(args, data_obj, model_func, init_model, rand_seed=0):
             avg_model = set_client_from_params(args, model_func().to(args.device), avg_model_params)
             # all_model = set_client_from_params(args, model_func(), np.mean(clnt_params_list, axis = 0))
 
-        elif args.mode in ["feddyn", "feddyn2"]:
+        elif args.mode in ["feddyn"]:
 
             cld_mdl_param_tensor = torch.tensor(cld_mdl_param, dtype=torch.float32, device=args.device)
             for clnt in selected_clnts:
@@ -170,8 +157,7 @@ def train(args, data_obj, model_func, init_model, rand_seed=0):
                 alpha_coef_adpt = args.alpha_coef / weight_list[clnt] # adaptive alpha coef
                 local_param_list_curr = torch.tensor(local_param_list[clnt], dtype=torch.float32, device=args.device)
                 clnt_models[clnt] = train_feddyn_mdl(args, model, model_func, alpha_coef_adpt, cld_mdl_param_tensor, 
-                                                     local_param_list_curr, trn_x, trn_y,
-                                                     print_per, data_obj.dataset)
+                                                     local_param_list_curr, trn_x, trn_y)
                 curr_model_par = get_mdl_params([clnt_models[clnt]], n_par)[0]
 
                 # No need to scale up hist terms. They are -\nabla/alpha and alpha is already scaled.
@@ -185,17 +171,12 @@ def train(args, data_obj, model_func, init_model, rand_seed=0):
             cld_model = set_client_from_params(args, model_func().to(args.device), cld_mdl_param) 
 
         if (i+1) % 10 == 0:
-
             all_model = set_client_from_params(args, model_func(), np.mean(clnt_params_list, axis = 0))
-
-            l1, a1 = get_acc_loss(args, data_obj.tst_x, data_obj.tst_y, avg_model, data_obj.dataset) 
-            # print("**** Communication sel %3d, Test Accuracy: %.4f, Loss: %.4f" %(i+1, acc_tst, loss_tst))
-            l2, a2 = get_acc_loss(args, cent_x, cent_y, avg_model, data_obj.dataset)
-            # print("**** Communication sel %3d, Cent Accuracy: %.4f, Loss: %.4f" %(i+1, acc_tst, loss_tst))
-            l3, a3 = get_acc_loss(args, data_obj.tst_x, data_obj.tst_y, all_model, data_obj.dataset)
-            # print("**** Communication all %3d, Test Accuracy: %.4f, Loss: %.4f" %(i+1, acc_tst, loss_tst))
-            l4, a4 = get_acc_loss(args, cent_x, cent_y, all_model, data_obj.dataset)
-            # print("**** Communication all %3d, Cent Accuracy: %.4f, Loss: %.4f" %(i+1, acc_tst, loss_tst))
+            for params in all_model.parameters(): params.requires_grad = True
+            l1, a1 = get_acc_loss(args, data_obj.tst_x, data_obj.tst_y, avg_model) 
+            l2, a2 = get_acc_loss(args, cent_x, cent_y, avg_model)
+            l3, a3 = get_acc_loss(args, data_obj.tst_x, data_obj.tst_y, all_model)
+            l4, a4 = get_acc_loss(args, cent_x, cent_y, all_model)
             print("Round {:<4}. Elapsed time: {:.0f}".format(i+1, time.time()-starttime))
             print("\t \t Train: {:.2f} \t Test: {:.2f}.".format(a4*100, a3*100))
             print("\t \t Train: {:.2f} \t Test: {:.2f}.".format(a2*100, a1*100))
