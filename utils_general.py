a2b1bb9ae251457a1f08be3498eb21b321b31165
ChiaHsiang0326 <0102030405jacky@gmail.com>
2023-05-18 19:48:25 +0800

    Update utils_general.py

--- utils_general.py
+++ utils_general.py
@@ -44,7 +44,6 @@ def set_client_from_params(args, mdl, params):
     for name, param in mdl.named_parameters():
         weights = param.data
         length = len(weights.reshape(-1))
-        # print(name, length)
         dict_param[name].data.copy_(torch.tensor(params[idx:idx+length].reshape(weights.shape)).to(args.device))
         idx += length
     
@@ -82,7 +81,7 @@ def __SetGrad(args, tensor, sign):
     if args.model_name in ["cnn"] or args.task in ["emnist"]:
         tensor.weight.requires_grad = sign
         tensor.bias.requires_grad = sign
-    elif args.model_name in ["resnet", "resnet34", "mobilenetv2"]:
+    elif args.model_name in ["resnet18", "resnet34", "mobilenetv2"]:
         for p1, p2 in tensor.named_parameters():
             p2.requires_grad = sign
 
@@ -220,25 +219,33 @@ def StopGradScheduler(args, model):
         if int(args.GU) == 113:
             if args.model_name == "resnet":
                 factor = (args.GU*100)%100
-                # print("Hi")
+
                 if  args.local_iter_count // factor <= 0: __SetGrad(args, model.model.layer2, False)
                 if  args.local_iter_count // factor <= 1: __SetGrad(args, model.model.layer3, False)
                 if  args.local_iter_count // factor <= 2: 
                     __SetGrad(args, model.model.layer4, False)
                     __SetGrad(args, model.model.fc, False)
 
-def train_model(args, model, trn_x, trn_y, learning_rate, batch_size, epoch, print_per, weight_decay, dataset_name):
+def GradModulator(args, model):
+    with torch.no_grad():
+        if int(args.GU) == 211:
+            factor = int(args.GU*10000)%10000 / 10000
+            model.fc3.weight.grad.data *= factor
+            model.fc3.bias.grad.data *= factor
+
+def train_model(args, model, trn_x, trn_y, dataset_name):
 
     n_trn = trn_x.shape[0]
-    trn_gen = data.DataLoader(Dataset(trn_x, trn_y, train=True, dataset_name=dataset_name), batch_size=batch_size, shuffle=True)
+    trn_gen = data.DataLoader(Dataset(trn_x, trn_y, train=True, dataset_name=dataset_name), batch_size=args.bs, shuffle=True)
     loss_fn = torch.nn.CrossEntropyLoss(reduction='sum')
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=weight_decay, momentum=args.momentum)
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, weight_decay=args.weight_decay, momentum=args.momentum)
     model.train(); model = model.to(args.device)
     args.local_iter_count = 0
-    for e in range(epoch):
+
+    for e in range(args.epoch):
         trn_gen_iter = trn_gen.__iter__()
-        for i in range(int(np.ceil(n_trn/batch_size))):
-            # print(int(np.ceil(n_trn/batch_size)))
+        for i in range(int(np.ceil(n_trn/args.bs))):
+            # print(int(np.ceil(n_trn/args.bs)))
             batch_x, batch_y = trn_gen_iter.__next__()
             batch_x = batch_x.to(args.device)
             batch_y = batch_y.to(args.device)
@@ -247,45 +254,56 @@ def train_model(args, model, trn_x, trn_y, learning_rate, batch_size, epoch, pri
             loss = loss_fn(y_pred, batch_y.reshape(-1).long()) / list(batch_y.size())[0]
             optimizer.zero_grad()
             loss.backward()
+#             GradModulator(args, model)
             torch.nn.utils.clip_grad_norm_(parameters=model.parameters(), max_norm=max_norm)
             optimizer.step()
             args.local_iter_count += 1
-        if (e+1) % print_per == 0: model.train()
+           
     for params in model.parameters(): params.requires_grad = False
     model.eval()            
     return model
 
 
 class FedDecorrLoss(nn.Module):
+
     def __init__(self):
         super(FedDecorrLoss, self).__init__()
         self.eps = 1e-8
+
     def _off_diagonal(self, mat):
+        # return a flattened view of the off-diagonal elements of a square matrix
         n, m = mat.shape
         assert n == m
         return mat.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
+
     def forward(self, x):
         N, C = x.shape
-        if N == 1: return 0.0
+        if N == 1:
+            return 0.0
+
         x = x - x.mean(dim=0, keepdim=True)
         x = x / torch.sqrt(self.eps + x.var(dim=0, keepdim=True))
+
         corr_mat = torch.matmul(x.t(), x)
-        loss = (self._off_diagonal(corr_mat).pow(2)).mean() / N
+
+        loss = (self._off_diagonal(corr_mat).pow(2)).mean()
+        loss = loss / N
+
         return loss
 
-def train_feddecorr_model(args, model, trn_x, trn_y, learning_rate, batch_size, epoch, print_per, weight_decay, dataset_name):
+def train_feddecorr_model(args, model, trn_x, trn_y, dataset_name):
 
     n_trn = trn_x.shape[0]
-    trn_gen = data.DataLoader(Dataset(trn_x, trn_y, train=True, dataset_name=dataset_name), batch_size=batch_size, shuffle=True) 
+    trn_gen = data.DataLoader(Dataset(trn_x, trn_y, train=True, dataset_name=dataset_name), batch_size=args.bs, shuffle=True) 
     loss_fn = torch.nn.CrossEntropyLoss(reduction='sum')
     loss_fn2 = FedDecorrLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=weight_decay, momentum=args.momentum)
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, weight_decay=args.weight_decay, momentum=args.momentum)
     model.train(); model = model.to(args.device)
     args.local_iter_count = 0
 
-    for e in range(epoch):
+    for e in range(args.epoch):
         trn_gen_iter = trn_gen.__iter__()
-        for i in range(int(np.ceil(n_trn/batch_size))):
+        for i in range(int(np.ceil(n_trn/args.bs))):
             batch_x, batch_y = trn_gen_iter.__next__()
             batch_x = batch_x.to(args.device)
             batch_y = batch_y.to(args.device)
@@ -298,35 +316,32 @@ def train_feddecorr_model(args, model, trn_x, trn_y, learning_rate, batch_size, 
 
             optimizer.zero_grad()
             loss.backward()
+            GradModulator(args, model)
 
             torch.nn.utils.clip_grad_norm_(parameters=model.parameters(), max_norm=max_norm)
             optimizer.step()
             args.local_iter_count += 1
         
-        if (e+1) % print_per == 0: model.train()
-
-        # break
-    
     for params in model.parameters(): params.requires_grad = False
     model.eval()            
     return model
 
-def train_feddyn_mdl(args, model, model_func, alpha_coef, avg_mdl_param, local_grad_vector, trn_x, trn_y, learning_rate, batch_size, epoch, print_per, weight_decay, dataset_name):
+def train_feddyn_mdl(args, model, model_func, alpha_coef, avg_mdl_param, local_grad_vector, trn_x, trn_y, dataset_name):
     n_trn = trn_x.shape[0]
-    trn_gen = data.DataLoader(Dataset(trn_x, trn_y, train=True, dataset_name=dataset_name), batch_size=batch_size, shuffle=True)
+    trn_gen = data.DataLoader(Dataset(trn_x, trn_y, train=True, dataset_name=dataset_name), batch_size=args.bs, shuffle=True)
     loss_fn = torch.nn.CrossEntropyLoss(reduction='sum')
     
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=alpha_coef+weight_decay, momentum=args.momentum)
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, weight_decay=alpha_coef+args.weight_decay, momentum=args.momentum)
     model.train(); model = model.to(args.device)
     
     n_par = get_mdl_params([model_func()]).shape[1]
 
     args.local_iter_count = 0
-    for e in range(epoch):
+    for e in range(args.epoch):
         # Training
         epoch_loss = 0
         trn_gen_iter = trn_gen.__iter__()
-        for i in range(int(np.ceil(n_trn/batch_size))):
+        for i in range(int(np.ceil(n_trn/args.bs))):
             batch_x, batch_y = trn_gen_iter.__next__()
             batch_x = batch_x.to(args.device)
             batch_y = batch_y.to(args.device)
@@ -357,30 +372,28 @@ def train_feddyn_mdl(args, model, model_func, alpha_coef, avg_mdl_param, local_g
 
             args.local_iter_count += 1
 
-        if (e+1) % print_per == 0: model.train()
-    
     for params in model.parameters(): params.requires_grad = False
     model.eval()
             
     return model
 
 ###
-def train_fedprox_mdl(args, model, avg_model_param_, mu, trn_x, trn_y, learning_rate, batch_size, epoch, print_per, weight_decay, dataset_name):
+def train_fedprox_mdl(args, model, avg_model_param_, mu, trn_x, trn_y, dataset_name):
     n_trn = trn_x.shape[0]
-    trn_gen = data.DataLoader(Dataset(trn_x, trn_y, train=True, dataset_name=dataset_name), batch_size=batch_size, shuffle=True)
+    trn_gen = data.DataLoader(Dataset(trn_x, trn_y, train=True, dataset_name=dataset_name), batch_size=args.bs, shuffle=True)
     loss_fn = torch.nn.CrossEntropyLoss(reduction='sum')
     
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=weight_decay, momentum=args.momentum)
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, weight_decay=args.weight_decay, momentum=args.momentum)
     model.train(); model = model.to(args.device)
     
     n_par = len(avg_model_param_)
     args.local_iter_count = 0
     
-    for e in range(epoch):
+    for e in range(args.epoch):
         # Training
         epoch_loss = 0
         trn_gen_iter = trn_gen.__iter__()
-        for i in range(int(np.ceil(n_trn/batch_size))):
+        for i in range(int(np.ceil(n_trn/args.bs))):
             batch_x, batch_y = trn_gen_iter.__next__()
             batch_x = batch_x.to(args.device)
             batch_y = batch_y.to(args.device)
@@ -412,16 +425,6 @@ def train_fedprox_mdl(args, model, avg_model_param_, mu, trn_x, trn_y, learning_
 
             args.local_iter_count += 1
 
-        if (e+1) % print_per == 0:
-            epoch_loss /= n_trn
-            if weight_decay != None:
-                # Add L2 loss to complete f_i
-                params = get_mdl_params([model], n_par)
-                epoch_loss += weight_decay/2 * np.sum(params * params)
-            
-            # print("Epoch %3d, Training Loss: %.4f" %(e+1, epoch_loss))
-            model.train()
-    
     # Freeze model
     for params in model.parameters():
         params.requires_grad = False
@@ -429,28 +432,28 @@ def train_fedprox_mdl(args, model, avg_model_param_, mu, trn_x, trn_y, learning_
             
     return model
 
-def train_scaffold_mdl(args, model, model_func, state_params_diff, trn_x, trn_y, learning_rate, batch_size, n_minibatch, print_per, weight_decay, dataset_name):
+def train_scaffold_mdl(args, model, model_func, state_params_diff, trn_x, trn_y, learning_rate, batch_size, n_minibatch, dataset_name):
     n_trn = trn_x.shape[0]
-    trn_gen = data.DataLoader(Dataset(trn_x, trn_y, train=True, dataset_name=dataset_name), batch_size=batch_size, shuffle=True)
+    trn_gen = data.DataLoader(Dataset(trn_x, trn_y, train=True, dataset_name=dataset_name), batch_size=args.bs, shuffle=True)
     loss_fn = torch.nn.CrossEntropyLoss(reduction='sum')
     
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=weight_decay, momentum=args.momentum)
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, weight_decay=args.weight_decay, momentum=args.momentum)
     model.train(); model = model.to(args.device)
     
     n_par = get_mdl_params([model_func()]).shape[1]
-    n_iter_per_epoch = int(np.ceil(n_trn/batch_size))
+    n_iter_per_epoch = int(np.ceil(n_trn/args.bs))
     epoch = np.ceil(n_minibatch / n_iter_per_epoch).astype(np.int64)
     count_step = 0
     is_done = False
     args.local_iter_count = 0
     
     step_loss = 0; n_data_step = 0
-    for e in range(epoch):
+    for e in range(args.epoch):
         # Training
         if is_done:
             break
         trn_gen_iter = trn_gen.__iter__()
-        for i in range(int(np.ceil(n_trn/batch_size))):
+        for i in range(int(np.ceil(n_trn/args.bs))):
             count_step += 1
             if count_step > n_minibatch:
                 is_done = True
@@ -471,7 +474,6 @@ def train_scaffold_mdl(args, model, model_func, state_params_diff, trn_x, trn_y,
                 if not isinstance(local_par_list, torch.Tensor): local_par_list = param.reshape(-1)
                 else: local_par_list = torch.cat((local_par_list, param.reshape(-1)), 0)
         
-            print(state_params_diff.sum())
             loss_algo = torch.sum(local_par_list * state_params_diff)
             loss = loss_f_i + loss_algo
 
@@ -480,18 +482,7 @@ def train_scaffold_mdl(args, model, model_func, state_params_diff, trn_x, trn_y,
             torch.nn.utils.clip_grad_norm_(parameters=model.parameters(), max_norm=max_norm) # Clip gradients
             optimizer.step()
             step_loss += loss.item() * list(batch_y.size())[0]; n_data_step += list(batch_y.size())[0]
-
-
-            if (count_step) % print_per == 0:
-                step_loss /= n_data_step
-                if weight_decay != None:
-                    # Add L2 loss to complete f_i
-                    params = get_mdl_params([model], n_par)
-                    step_loss += (weight_decay)/2 * np.sum(params * params)
-                # print("Step %3d, Training Loss: %.4f" %(count_step, step_loss))
-                step_loss = 0; n_data_step = 0
-                model.train()
-    
+   
     # Freeze model
     for params in model.parameters():
         params.requires_grad = False
