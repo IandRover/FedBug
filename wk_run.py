2abc315f6e8f5abc48030e176ded34234e0a4903
ChiaHsiang0326 <0102030405jacky@gmail.com>
2023-05-18 19:47:50 +0800

    Update wk_run.py

--- wk_run.py
+++ wk_run.py
@@ -6,21 +6,21 @@ import argparse, datetime, copy, time
 parser = argparse.ArgumentParser()
 parser.add_argument('--mode', type = str, default = "fedavg", 
                     choices=["feddyn", "scaffold", "fedavg", "fedprox",
-                             "fedexp", "fedcm", "feddecorr",
+                             "fedexp", "fedcm", "feddecorr", "feddyn2",
                              "fedadam", "fedadagrad", "fedavgm"])
 parser.add_argument('--epoch', type = int, default = 5)
-parser.add_argument('--model_name', type = str, default = "resnet", choices=["cnn", "resnet18", "resnet34"])
+parser.add_argument('--model_name', type = str, default = "cnn", choices=["cnn", "resnet18", "mobilenetv2"])
 parser.add_argument('--norm', type = str, default = "no")
 
 
 parser.add_argument('--task', type = str, default = "CIFAR100", 
-                    choices=["CIFAR10", "CIFAR100", "TinyImageNet"])
+                    choices=["CIFAR10", "CIFAR100", "mnist", "emnist", "TinyImageNet"])
 parser.add_argument('--balance', type = float, default = 0.,
                     help="0: balanced | 0-1: sampling rate",
                     choices=["CIFAR10", "CIFAR100", "mnist", "emnist"])
 parser.add_argument('--dist', type = float, default = 0.0,
                     help="0: iid | 0-1: non-iid with unbalanced_sgm")
-parser.add_argument('--n_clients', type = int, default = 10)
+parser.add_argument('--n_clients', type = int, default = 100)
 parser.add_argument('--act_prob', type = float, default = .1)
 parser.add_argument('--lr', type = float, default = 0.1)
 parser.add_argument('--lr_update_mode', type = str, default = "exp",
@@ -52,7 +52,7 @@ max_norm = 10
 
 # ==============================================
 if args.weight_decay != 1e-3: args.extension += "_WD" + format(args.weight_decay, '.0e')[-1]
-if args.model_name == "resnet": args.extension += "_resnet"
+if args.model_name == "resnet18": args.extension += "_resnet"
 if args.model_name == "mobilenet": args.extension += "_mobilenet"
 if args.GU == 0: args.GU = 0
 if args.GU != 0:
@@ -63,12 +63,10 @@ if args.momentum != 0:
 
 # ==============================================
 weight_decay       = args.weight_decay
-batch_size         = 50
 lr_decay_per_round = args.lr_decay_per_round
-epoch              = args.epoch
 print_per          = 5
 args.eps = 1e-5
-args.batch_size = 50
+args.bs = 50
 
 if args.task == "TinyImageNet":
     if args.act_prob == 0.01:
@@ -77,7 +75,7 @@ if args.task == "TinyImageNet":
         args.com_amount = 300
     if args.act_prob == 1.:
         args.com_amount = 500
-    args.batch_size = 50
+    
 
     if args.n_clients == 10 and args.epoch == 5: 
         args.com_amount = 100
@@ -91,7 +89,7 @@ if args.task == "CIFAR100":
         args.com_amount = 500
     if args.act_prob == 1.:
         args.com_amount = 500
-    args.batch_size = 50
+    
 
     if args.n_clients == 10 and args.epoch == 5: 
         args.com_amount = 100
@@ -103,7 +101,7 @@ if args.task == "CIFAR10":
         args.com_amount = 500
     if args.act_prob == 1.:
         args.com_amount = 500
-    args.batch_size = 50
+    
 
     if args.n_clients == 10:
         args.com_amount = 200
@@ -111,21 +109,21 @@ if args.task == "CIFAR10":
 if args.task == "emnist":
     if args.act_prob == 1:
         args.com_amount = 500
-        args.batch_size = 50
+        
             
     if args.act_prob == 0.01:
         args.com_amount = 500
-        args.batch_size = 50
+        
 
-args.extension += "__LR" + str(args.lr)
+args.extension += "_LR" + str(args.lr)
 if args.balance == 0.: args.balance = 0
 if args.dist == 0.: 
     args.dist = 0
 
 # ==============================================
-if args.task == "CIFAR100" and args.model_name == "resnet":
+if args.task == "CIFAR100" and args.model_name == "resnet18":
     savepath = 'Output_GU_C100_Resnet/{}-{}-B{}-D{}-N{}-P{}_{}'.format(args.task, args.mode, args.balance, args.dist, args.n_clients, args.act_prob, args.extension)
-elif args.task == "TinyImageNet" and args.model_name == "resnet":
+elif args.task == "TinyImageNet" and args.model_name == "resnet18":
     savepath = 'Output_GU_TIN_Resnet/{}-{}-B{}-D{}-N{}-P{}_{}'.format(args.task, args.mode, args.balance, args.dist, args.n_clients, args.act_prob, args.extension)
 elif args.task == "CIFAR100" and args.model_name == "mobilenetv2":
     savepath = 'Output_GU_C100_Mobilenetv2/{}-{}-B{}-D{}-N{}-P{}_{}'.format(args.task, args.mode, args.balance, args.dist, args.n_clients, args.act_prob, args.extension)
@@ -150,9 +148,9 @@ if os.path.exists(args.savename):
     print("The seed already exists.")
     print("The path is ", args.savename)
     print()
-    time.sleep(0.1)
-    import sys
-    sys.exit()
+    # time.sleep(0.1)
+    # import sys
+    # sys.exit()
 
 # ==============================================
 model_func = lambda : client_model(args.task.lower(), args)
@@ -172,8 +170,8 @@ if args.mode == "feddyn":
     args.alpha_coef = 1e-2
 elif args.mode == "scaffold":
     n_data_per_client = np.concatenate(data_obj.clnt_x, axis=0).shape[0] / args.n_clients
-    n_iter_per_epoch  = np.ceil(n_data_per_client/batch_size)
-    args.n_minibatch = (epoch*n_iter_per_epoch).astype(np.int64)
+    n_iter_per_epoch  = np.ceil(n_data_per_client/args.bs)
+    args.n_minibatch = (args.epoch*n_iter_per_epoch).astype(np.int64)
 elif args.mode == "fedprox":
     args.mu = 1e-4
 elif args.mode in ["fedadam", "fedadagrad", "fedadagrad1", "fedadagrad2", "fedavgm"]:
@@ -181,6 +179,5 @@ elif args.mode in ["fedadam", "fedadagrad", "fedadagrad1", "fedadagrad2", "fedav
     args.b1 = 0.9
     args.b2 = 0.99
 
-train(args=args, data_obj=data_obj, learning_rate=args.lr, batch_size=batch_size,
-             epoch=epoch, print_per=print_per, weight_decay=weight_decay,
-             model_func=model_func, init_model=init_model, lr_decay_per_round=lr_decay_per_round)
+train(args=args, data_obj=data_obj, 
+             model_func=model_func, init_model=init_model)
